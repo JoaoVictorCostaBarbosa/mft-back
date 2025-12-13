2c47a81a823bea724d955308e34a3b15b89bc6d5
Joao Victor <joaovictorcostabarbosac@gmail.com>
2025-12-13 18:08:06 -0300

    domain: update auth token data structure

--- src/domain/auth/token_data.rs
+++ src/domain/auth/token_data.rs
@@ -1,10 +1,12 @@
 use crate::domain::enums::role::Role;
 
+#[derive(Debug, Clone)]
 pub struct AccessTokenData {
     pub user_id: String,
     pub role: Role,
 }
 
+#[derive(Debug, Clone)]
 pub struct RefreshTokenData {
     pub user_id: String,
 }
