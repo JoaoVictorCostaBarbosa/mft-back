62c1d6eae3eaaede307756ffe1112f248ce7728c
Joao Victor <joaovictorcostabarbosac@gmail.com>
2025-12-21 10:40:11 -0300

    update: postgres repository mod

--- src/infrastructure/repositories/postgres/mod.rs
+++ src/infrastructure/repositories/postgres/mod.rs
@@ -3,15 +3,21 @@ use crate::{
         pending_change_repository::PendingChangesRepository,
         pending_user_repository::PendingUserRepository,
     },
-    domain::repositories::user_repository::UserRepository,
+    domain::repositories::{
+        measurement_repository::MeasurementRepository, user_repository::UserRepository,
+    },
     infrastructure::repositories::postgres::{
-        pending_change_repository_sqlx::PendingChangeRepositorySqlx, pending_user_repository_sqlx::PendingUserRepositorySqlx, user_repository_sqlx::UserRepositorySQLx
+        measurement_repository_sqlx::MeasurementRepositorySqlx,
+        pending_change_repository_sqlx::PendingChangeRepositorySqlx,
+        pending_user_repository_sqlx::PendingUserRepositorySqlx,
+        user_repository_sqlx::UserRepositorySQLx,
     },
 };
 use sqlx::PgPool;
 use std::sync::Arc;
 
 pub mod error;
+pub mod measurement_repository_sqlx;
 pub mod pending_change_repository_sqlx;
 pub mod pending_user_repository_sqlx;
 pub mod user_repository_sqlx;
@@ -20,6 +26,7 @@ pub struct RepositoryBundle {
     pub user_repo: Arc<dyn UserRepository>,
     pub pending_user_repo: Arc<dyn PendingUserRepository>,
     pub pending_change_repo: Arc<dyn PendingChangesRepository>,
+    pub measurement_repo: Arc<dyn MeasurementRepository>,
 }
 
 impl RepositoryBundle {
@@ -28,6 +35,7 @@ impl RepositoryBundle {
             user_repo: Arc::new(UserRepositorySQLx::new(pool.clone())),
             pending_user_repo: Arc::new(PendingUserRepositorySqlx::new(pool.clone())),
             pending_change_repo: Arc::new(PendingChangeRepositorySqlx::new(pool.clone())),
+            measurement_repo: Arc::new(MeasurementRepositorySqlx::new(pool.clone())),
         }
     }
 }
