5eaa15c39dec775ccd288631d9a1270391d247e2
JoaoVictorCostaBarbosa <joaovictorcostabarbosac@gmail.com>
2026-05-31 20:47:31 -0300

    docs(swagger): document workout plan creation

--- src/adapters/http/handlers/workout_plan/create_wp.rs
+++ src/adapters/http/handlers/workout_plan/create_wp.rs
@@ -9,6 +9,21 @@ use crate::{
 };
 use axum::{Json, extract::State, http::StatusCode, response::IntoResponse};
 
+#[utoipa::path{
+    post,
+    path = "/api/workout-plans",
+    request_body = WorkoutPlanRequestDTO,
+    responses(
+        (status = 201, description = "Workout plan created", body = WorkoutPlanResponseDTO),
+        (status = 403, description = "denied permission"),
+        (status = 422, description = "unprocessable entity"),
+        (status = 500, description = "internal server error"),
+    ),
+    security(
+        ("bearer_auth" = [])
+    ),
+    tag = "Workout Plans"
+}]
 pub async fn create_workout_plan_handler(
     State(state): State<AppState>,
     CurrentUser(current_user): CurrentUser,
