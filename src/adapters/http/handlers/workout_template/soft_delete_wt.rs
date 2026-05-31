1a8e606b306225e7d973fbaff8639dff69b5a44b
JoaoVictorCostaBarbosa <joaovictorcostabarbosac@gmail.com>
2026-05-31 20:47:31 -0300

    docs(swagger): document workout template soft delete

--- src/adapters/http/handlers/workout_template/soft_delete_wt.rs
+++ src/adapters/http/handlers/workout_template/soft_delete_wt.rs
@@ -9,6 +9,23 @@ use axum::{
 };
 use uuid::Uuid;
 
+#[utoipa::path{
+    delete,
+    path = "/api/workout-templates/{workout_id}/soft-delete",
+    params(
+        ("workout_id" = Uuid, description = "Workout template ID to be soft deleted", example = "b728b759-4d32-4148-936e-d9036c071d72"),
+    ),
+    responses(
+        (status = 204, description = "no content"),
+        (status = 403, description = "denied permission"),
+        (status = 404, description = "not found"),
+        (status = 500, description = "internal server error"),
+    ),
+    security(
+        ("bearer_auth" = [])
+    ),
+    tag = "Workout Templates"
+}]
 pub async fn soft_delete_workout_template_handler(
     State(state): State<AppState>,
     CurrentUser(current_user): CurrentUser,
