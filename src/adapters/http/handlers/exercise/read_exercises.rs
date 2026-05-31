e5e1a552b781df0dfeb699eebf0ebf78e05c26c7
JoaoVictorCostaBarbosa <joaovictorcostabarbosac@gmail.com>
2026-05-31 20:47:30 -0300

    docs(swagger): document exercise listing

--- src/adapters/http/handlers/exercise/read_exercises.rs
+++ src/adapters/http/handlers/exercise/read_exercises.rs
@@ -7,6 +7,19 @@ use crate::{
 };
 use axum::{Json, extract::State, http::StatusCode, response::IntoResponse};
 
+#[utoipa::path{
+    get,
+    path = "/api/exercises",
+    responses(
+        (status = 200, description = "Exercises found", body = [ExerciseResponseDTO]),
+        (status = 403, description = "denied permission"),
+        (status = 500, description = "internal server error"),
+    ),
+    security(
+        ("bearer_auth" = [])
+    ),
+    tag = "Exercises"
+}]
 pub async fn read_exercises_handler(
     State(state): State<AppState>,
     CurrentUser(current_user): CurrentUser,
