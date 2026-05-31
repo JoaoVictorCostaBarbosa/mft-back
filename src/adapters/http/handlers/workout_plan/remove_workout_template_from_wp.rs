5cd7035f34b145deed48da4a2b0ead91226da676
JoaoVictorCostaBarbosa <joaovictorcostabarbosac@gmail.com>
2026-05-31 20:47:31 -0300

    docs(swagger): document plan template removal

--- src/adapters/http/handlers/workout_plan/remove_workout_template_from_wp.rs
+++ src/adapters/http/handlers/workout_plan/remove_workout_template_from_wp.rs
@@ -9,6 +9,24 @@ use axum::{
 };
 use uuid::Uuid;
 
+#[utoipa::path{
+    delete,
+    path = "/api/workout-plans/{workout_plan_id}/workout-template/{workout_template_id}",
+    params(
+        ("workout_plan_id" = Uuid, description = "Workout plan ID", example = "b728b759-4d32-4148-936e-d9036c071d72"),
+        ("workout_template_id" = Uuid, description = "Workout template ID", example = "b728b759-4d32-4148-936e-d9036c071d72"),
+    ),
+    responses(
+        (status = 204, description = "Workout template removed from workout plan"),
+        (status = 403, description = "denied permission"),
+        (status = 404, description = "not found"),
+        (status = 500, description = "internal server error"),
+    ),
+    security(
+        ("bearer_auth" = [])
+    ),
+    tag = "Workout Plans"
+}]
 pub async fn remove_workout_template_from_workout_plan_handler(
     State(state): State<AppState>,
     CurrentUser(current_user): CurrentUser,
