0453c7cc60b37ebebc2e7592fce394ee63b9eb42
JoaoVictorCostaBarbosa <joaovictorcostabarbosac@gmail.com>
2026-05-31 20:47:30 -0300

    docs(swagger): document exercise soft delete

--- src/adapters/http/handlers/exercise/soft_delete_exercise.rs
+++ src/adapters/http/handlers/exercise/soft_delete_exercise.rs
@@ -9,6 +9,23 @@ use axum::{
 };
 use uuid::Uuid;
 
+#[utoipa::path{
+    patch,
+    path = "/api/exercises/{id}/soft-delete",
+    params(
+        ("id" = Uuid, description = "Exercise ID to be soft deleted", example = "b728b759-4d32-4148-936e-d9036c071d72"),
+    ),
+    responses(
+        (status = 204, description = "no content"),
+        (status = 403, description = "denied permission"),
+        (status = 404, description = "not found"),
+        (status = 500, description = "internal server error"),
+    ),
+    security(
+        ("bearer_auth" = [])
+    ),
+    tag = "Exercises"
+}]
 pub async fn soft_delete_exercise_handler(
     State(state): State<AppState>,
     CurrentUser(current_user): CurrentUser,
