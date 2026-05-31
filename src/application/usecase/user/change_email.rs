64037060fd36793bb585d328b679ae1d3f3c8532
JoaoVictorCostaBarbosa <joaovictorcostabarbosac@gmail.com>
2026-05-31 20:49:37 -0300

    style(user): format change email usecase

--- src/application/usecase/user/change_email.rs
+++ src/application/usecase/user/change_email.rs
@@ -1,14 +1,17 @@
 use crate::{
     application::{
-        dtos::user::{
-            email_change_request::EmailChangeRequest, pending_change::PendingChange,
-        },
+        dtos::user::{email_change_request::EmailChangeRequest, pending_change::PendingChange},
         interfaces::pending_change_repository::PendingChangesRepository,
     },
     domain::{
-        commands::user_commands::UserUpdateFilds, entities::user::User, enums::role::Role, errors::{
+        commands::user_commands::UserUpdateFilds,
+        entities::user::User,
+        enums::role::Role,
+        errors::{
             domain_error::DomainError, permission_error::PermissionError, user_error::UserError,
-        }, repositories::user_repository::UserRepository, value_objects::email_vo::Email
+        },
+        repositories::user_repository::UserRepository,
+        value_objects::email_vo::Email,
     },
 };
 use std::sync::Arc;
